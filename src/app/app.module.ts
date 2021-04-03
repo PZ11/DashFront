98c0c2a58ef7d66463ae0a6eee69cd58e3e10845
yuepeng <yuepeng@yuepengs-Mac-mini.local>
2021-04-03 12:05:50 -0400

    add order table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule} from '@angular/router';
-
 import { ChartsModule} from 'ng2-charts'
 
 import {appRoutes} from '../routes'
