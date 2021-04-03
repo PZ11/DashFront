054b0cfb8f2b1b93ee2036ce8d3778bbd82d3e7d
yuepeng <yuepeng@yuepengs-Mac-mini.local>
2021-04-02 20:56:40 -0400

    sales chart added

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {LINE_CHART_CLORS} from '../../shared/chart.colors'
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [35,14,46,23,38,56], label: 'Sentiment Analysis'},
+  { data: [15,18,26,13,26,28], label: 'Image Recognition'},
+  { data: [55,34,49,53,66,62], label: 'Forecasting'},
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,17 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_CLORS
+
 
   ngOnInit(): void {
   }
